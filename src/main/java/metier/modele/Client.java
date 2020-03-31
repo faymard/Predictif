3b73162e1aee75fe9fc18c5128cd292f6948cd64
Florian <florian.aymard@gmail.com>
2020-03-31 15:03:49 +0200

    Classe modèle Client

--- src/main/java/metier/modele/Client.java
+++ src/main/java/metier/modele/Client.java
@@ -5,10 +5,53 @@
  */
 package metier.modele;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 /**
  *
  * @author Florian
  */
-public class Client {
+@Entity
+public class Client extends Utilisateur implements Serializable {
+    
+    private String address;
+    private String civilite;
+    
+    public Client() {
+        
+    }
+
+    public Client(String address, String civilite, String nom, String prenom, String mail, String telephone, String motDePasse) {
+        super(nom, prenom, mail, telephone, motDePasse);
+        this.address = address;
+        this.civilite = civilite;
+    }
+    
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCivilite() {
+        return civilite;
+    }
+
+    public void setCivilite(String civilite) {
+        this.civilite = civilite;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "address=" + address + ", civilite=" + civilite + '}';
+    }
+    
     
 }
