137d715f5fcbf143b8ec53eea19bbac0964e2c1e
Pierre Huchot <pierre.huchot@gmail.com>
2020-03-31 15:21:56 +0200

    Ajout de la dao Employe

--- src/main/java/dao/EmployeDao.java
+++ src/main/java/dao/EmployeDao.java
@@ -5,10 +5,42 @@
  */
 package dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import metier.modele.Employe;
+
 /**
  *
  * @author Pierre
  */
 public class EmployeDao {
+    public void creer(Employe employe) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.persist(employe);
+    }
+    
+    public Employe chercherParId(Long employeId) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        return em.find(Employe.class, employeId); // renvoie null si l'identifiant n'existe pas
+    }
+    
+    public Employe chercherParMail(String clientMail) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        TypedQuery<Employe> query = em.createQuery("SELECT e FROM Utilisateur e WHERE e.mail = :mail", Employe.class);
+        query.setParameter("mail", clientMail); // correspond au paramètre ":mail" dans la requête
+        List<Employe> employes = query.getResultList();
+        Employe result = null;
+        if (!employes.isEmpty()) {
+            result = employes.get(0); // premier de la liste
+        }
+        return result;
+    }
+    
+    public List<Employe> listerEmployes() {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        TypedQuery<Employe> query = em.createQuery("SELECT c FROM Utilisateur c ORDER BY c.nom ASC, c.prenom ASC", Employe.class);
+        return query.getResultList();
+    }
     
 }
