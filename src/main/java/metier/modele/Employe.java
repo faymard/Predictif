fb9bd464ba90d6ea2bec1dc99985fd3a63158bb8
Pierre Huchot <pierre.huchot@gmail.com>
2020-03-31 15:06:46 +0200

    Modele employe + dao associée

---
+++ src/main/java/metier/modele/Employe.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier.modele;
+
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author Pierre
+ */
+public class Employe extends Utilisateur implements Serializable {
+
+    private char genre;
+
+    public Employe() {
+    }
+
+    public Employe(char genre, String nom, String prenom, String mail, String telephone, String motDePasse) {
+        super(nom, prenom, mail, telephone, motDePasse);
+        this.genre = genre;
+    }
+
+    
+    
+    public char getGenre() {
+        return genre;
+    }
+
+    public void setGenre(char genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" + "genre=" + genre + '}';
+    }
+
+}
