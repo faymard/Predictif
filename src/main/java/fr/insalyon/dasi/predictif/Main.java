d8ce67b13b3ad4b1bfe34d446bbff3ba8ba5edf8
Pierre Huchot <pierre.huchot@gmail.com>
2020-03-31 16:37:40 +0200

    Insertion des employes

--- src/main/java/fr/insalyon/dasi/predictif/Main.java
+++ src/main/java/fr/insalyon/dasi/predictif/Main.java
@@ -6,7 +6,9 @@
 package fr.insalyon.dasi.predictif;
 
 import metier.modele.Client;
+import metier.modele.Employe;
 import dao.ClientDao;
+import dao.EmployeDao;
 import dao.JpaUtil;
 
 /**
@@ -14,7 +16,7 @@ import dao.JpaUtil;
  * @author Florian
  */
 public class Main {
-    
+
     public static void testInscriptionClient() {
         Client c1 = new Client("6 rue des zobs", "Monsieur", "Pastissier", "Michel", "pastis@jaune.fr", "0651515151", "jaune");
         //System.out.println(c1);
@@ -36,11 +38,36 @@ public class Main {
         JpaUtil.fermerContextePersistance();
         System.out.println(c_test);
     }
-    
+
+    public static void insererEmployes() {
+        JpaUtil.creerContextePersistance();
+
+        EmployeDao daoE = new EmployeDao();
+
+        Employe[] te = new Employe[3];
+
+        te[0] = new Employe('M', "CEURLOT", "Florian", "florian.ceurlot@hotmail.fr", "0948039309", "1234");
+        te[1] = new Employe('M', "PROOTE", "Thiebaud", "thiebaud.proote@free.fr", "0847090862", "5678");
+        te[2] = new Employe('F', "BREDU", "Sara", "sara.bredu@laposte.net", "0203896476", "91011");
+
+        try {
+            JpaUtil.ouvrirTransaction();
+            for (Employe e : te) {
+                daoE.creer(e);
+            }
+            JpaUtil.validerTransaction();
+        } catch (Exception ex) {
+            JpaUtil.annulerTransaction();
+
+        }
+
+        JpaUtil.fermerContextePersistance();
+    }
+
     public static void main(String[] args) {
         JpaUtil.init();
         testInscriptionClient();
+        insererEmployes();
     }
-    
-    
+
 }
